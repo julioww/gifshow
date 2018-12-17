ad62350bc0bbc66826650b0f397e45fd218297c6
Julio Abreu <juliow@gmail.com>
2018-12-17 21:43:53 +0100

    added GifModel tests

---
+++ GifShow/GifShowTests/Models/GifModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  GifModelTests.swift
+//  GifShowTests
+//
+//  Created by Julio Cezar Freitas de Abreu on 17.12.18.
+//  Copyright © 2018 Julio Cezar Freitas de Abreu. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import SwiftyJSON
+
+class GifModelTests: XCTestCase {
+    
+    //TODO: Move to a helper class and update the other thats to use that
+    func getJsonFromFile(resource: String, ext: String) -> JSON? {
+        let bundle = Bundle(for: type(of: self))
+        guard let file = bundle.url(forResource: resource, withExtension: ext),
+            let data = try? Data(contentsOf: file) else {
+                return nil
+        }
+        let json = try? JSON(data: data)
+        return json
+    }
+    
+    func testGif() {
+        let json = getJsonFromFile(resource: "first_result", ext: "json")
+        let itemsPerpage = json!["pagination"]["count"]
+        var mocks:[Gif] = []
+        for item in json!["data"].arrayValue {
+            mocks.append(Gif(json: item))
+        }
+        
+        XCTAssertEqual(mocks.count, itemsPerpage.intValue)
+    }
+}
